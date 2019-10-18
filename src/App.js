f3b19671eb420347705a6a13e2e2187b89d68d86
Javier Cervelló <javi.cervello@gmail.com>
2019-10-18 17:41:04 +0200

    BrowserRoute structure created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/loquesea">loquesea</Route>
+          <Route exact path="/otra">otra</Route>
+          <Route  path="/">me sale por que hace match con la barra</Route>
+          <Route path="*">este es el global</Route>
+
+
+        </Switch>
+      
+      </BrowserRouter>
     </div>
   );
 }
